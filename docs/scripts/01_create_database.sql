87313c0bcee1662dddb0e591326ff38f99736bf6
Orlando J Betancourth <orlando.betancourth@gmail.com>
2019-10-21 19:56:24 -0600

    Primer Commit MVC mysql y View

--- docs/scripts/01_create_database.sql
+++ docs/scripts/01_create_database.sql
@@ -1,3 +1,6 @@
 CREATE SCHEMA `nw201903` ;
-CREATE USER 'demouser'@'%' IDENTIFIED BY 'carm3l1t4s';
+-- Mysql < 8
+-- CREATE USER 'demouser'@'%' IDENTIFIED BY 'carm3l1t4s';
+-- Mysql >= 8  or MariaDB
+CREATE USER 'nw201903'@'%' IDENTIFIED WITH mysql_native_password BY 'carm3l1t4s';
 GRANT ALL ON nw201903.* TO 'demouser'@'%';
